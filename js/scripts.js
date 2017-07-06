259002ba1f428012a081bd20ea4af04e2805d880
Kimberly Lu and Paul Guevarra <paul@>
2017-07-06 10:23:53 -0700

    entered variables to js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var name = $("#person1").val();
+
+var name = $("#food").val();
+
+var flavor = $("input:radio[name=music]:checked").val();
+
+var flavor = $("input:radio[name=color]:checked").val();
